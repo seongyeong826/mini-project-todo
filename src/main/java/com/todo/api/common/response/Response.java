24808cda71060d2883522cd29496cff9b825d92e
sunveloper <96734777+seongyeong826@users.noreply.github.com>
2024-06-21 23:16:19 +0900

    refactor: response의 success 필드 제거
    status 상태값으로 충분히 해결 가능

--- src/main/java/com/todo/api/common/response/Response.java
+++ src/main/java/com/todo/api/common/response/Response.java
@@ -6,27 +6,24 @@ import lombok.Getter;
 @Getter
 public class Response<T> {
 
-    private final boolean success;
     private final T data;
     private final ErrorResponse error;
 
-    private Response(boolean success, T data) {
-        this.success = success;
+    private Response(T data) {
         this.data = data;
         this.error = null;
     }
 
-    private Response(boolean success, ErrorResponse error) {
-        this.success = success;
+    private Response(ErrorResponse error) {
         this.data = null;
         this.error = error;
     }
 
     public static <T> Response<T> success(T data) {
-        return new Response<>(true, data);
+        return new Response<>(data);
     }
 
     public static <T> Response<T> fail(ExceptionCode code) {
-        return new Response<>(false, new ErrorResponse(code.getCode(), code.getMessage()));
+        return new Response<>(new ErrorResponse(code.getCode(), code.getMessage()));
     }
 }
